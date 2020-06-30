c1d6d8b3860cf4893590df1f830fa959b0f2e07e
torla <munford.ns@gmail.com>
2020-06-30 17:04:32 +0200

    more

--- main.py
+++ main.py
@@ -1,7 +1,127 @@
 import psycopg2
 
+
+def create_table(connection):
+    cursor = connection.cursor()
+
+    try:
+
+        create_table_query = '''CREATE TABLE students
+              (ID SERIAL   PRIMARY KEY ,
+              NAME           TEXT    NOT NULL,
+              SURNAME         TEXT NOT NULL,
+              unique(NAME,SURNAME)); '''
+
+        cursor.execute(create_table_query)
+
+        create_table_query = '''CREATE TABLE courses
+              (ID SERIAL  PRIMARY KEY ,
+              NAME           TEXT    NOT NULL,
+              DESCR        TEXT NOT NULL); '''
+
+        cursor.execute(create_table_query)
+
+        create_table_query = '''CREATE TABLE booking
+              (STUDENT_ID BIGINT  ,
+              COURSE_ID           BIGINT ,
+              PRIMARY KEY (STUDENT_ID,COURSE_ID)
+              ); '''
+
+        cursor.execute(create_table_query)
+
+        connection.commit()
+    except (Exception, psycopg2.Error) as error:
+        print("Error creating tables", error)
+    finally:
+        cursor.close()
+
+def insert_names(connection):
+    cursor = connection.cursor()
+
+    try:
+
+        nomi = [("Marco", "Torlaschi"), ("Roberta", "Latini"), ("Giorgo", "Franzini")]
+
+        insert = "INSERT INTO students (NAME,SURNAME) VALUES (%s,%s)"
+
+        for n in nomi:
+            cursor.execute(insert, n)
+
+        connection.commit()
+    except (Exception, psycopg2.Error) as error:
+        print("Error inserting names", error)
+    finally:
+        cursor.close()
+def insert_courses(connection):
+    cursor = connection.cursor()
+
+    try:
+
+        corsi = [("SQL", "corso sql"), ("SPRING", "corso spring")]
+
+        insert = "INSERT INTO courses (NAME,DESCR) VALUES (%s,%s)"
+
+        for n in corsi:
+            cursor.execute(insert, n)
+
+        connection.commit()
+    except (Exception, psycopg2.Error) as error:
+        print("Error inserting names", error)
+    finally:
+        cursor.close()
+
+def insert_booking(connection):
+    cursor = connection.cursor()
+
+    try:
+
+
+        corsi = [("SQL", "corso sql"), ("SPRING", "corso spring")]
+
+        insert = "INSERT INTO courses (NAME,DESCR) VALUES (%s,%s)"
+
+        for n in corsi:
+            cursor.execute(insert, n)
+
+        connection.commit()
+    except (Exception, psycopg2.Error) as error:
+        print("Error inserting names", error)
+    finally:
+        cursor.close()
+
+
+def delate_names(connetion):
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute("DELETE FROM  students where name='Roberta' ")
+
+
+        # connection.rollback()
+        connection.commit()
+    except (Exception, psycopg2.Error) as error:
+        print("Error deleting name", error)
+    finally:
+        cursor.close()
+
+def drop_tables(connetion):
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute("DROP TABLE  students  ")
+        cursor.execute("DROP TABLE  courses  ")
+        cursor.execute("DROP TABLE  booking  ")
+        connection.commit()
+    except (Exception, psycopg2.Error) as error:
+        print("Error deleting name", error)
+    finally:
+        cursor.close()
+
+
 if __name__ == '__main__':
     print("esempi sql")
+
+
 
     try:
         connection = psycopg2.connect(user="postgres",
@@ -10,43 +130,31 @@ if __name__ == '__main__':
                                       port="5432",
                                       database="sample")
 
+
+        #drop_tables(connection)
+
+        create_table(connection)
+
         cursor = connection.cursor()
 
-        create_table_query = '''CREATE TABLE students
-              (ID SERIAL   ,
-              NAME           TEXT    NOT NULL,
-              SURNAME         TEXT NOT NULL); '''
+        insert_names(connection)
 
-        cursor.execute(create_table_query)
-
-        nomi = [("Marco", "Torlaschi"), ("Roberta", "Latini"), ("Giorgo", "Franzini")]
-
-        insert = "INSERT INTO students (name,surname) VALUES (%s,%s)"
-
-        connection.commit()
-
-        for n in nomi:
-            cursor.execute(insert, n)
+        insert_courses(connection)
 
         cursor.execute("SELECT * FROM  students  ")
         for r in cursor.fetchall():
             print(r, "\n")
 
-        print("adsd")
+        cursor.execute("SELECT * FROM  courses  ")
+        for r in cursor.fetchall():
+            print(r, "\n")
 
-        cursor.execute("DELETE FROM  students where name='Roberta' ")
-
-        cursor.execute("SELECT * FROM  students  ")
+        cursor.execute("SELECT * FROM  booking  ")
         for r in cursor.fetchall():
             print(r, "\n")
 
 
-        cursor.execute("DELETE FROM  students  ")
-
-        cursor.execute("DROP TABLE  students  ")
-
-        connection.commit()
-
+        drop_tables(connection)
 
 
     except (Exception, psycopg2.Error) as error:
@@ -54,6 +162,5 @@ if __name__ == '__main__':
     finally:
         # closing database connection.
         if (connection):
-            cursor.close()
             connection.close()
             print("PostgreSQL connection is closed")
