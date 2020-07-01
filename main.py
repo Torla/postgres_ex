23cee56776c54fffb4afa24a769cfb8ab0e5d0a2
torla <munford.ns@gmail.com>
2020-07-01 16:23:03 +0200

    added data

--- main.py
+++ main.py
@@ -2,16 +2,19 @@ import psycopg2
 
 
 def create_table(connection):
+    # il cursore è utillizato esequire comandi e accedere alla risposta
     cursor = connection.cursor()
 
     try:
 
+        # prepara il comando
         create_table_query = '''CREATE TABLE students
               (ID SERIAL   PRIMARY KEY ,
               NAME           TEXT    NOT NULL,
               SURNAME         TEXT NOT NULL,
               unique(NAME,SURNAME)); '''
 
+        # eseguo il comando
         cursor.execute(create_table_query)
 
         create_table_query = '''CREATE TABLE courses
@@ -24,16 +27,21 @@ def create_table(connection):
         create_table_query = '''CREATE TABLE booking
               (STUDENT_ID BIGINT  ,
               COURSE_ID           BIGINT ,
-              PRIMARY KEY (STUDENT_ID,COURSE_ID)
+              PRIMARY KEY (STUDENT_ID,COURSE_ID),
+              FOREIGN KEY (STUDENT_ID) REFERENCES students(ID) MATCH FULL ON DELETE CASCADE,
+              FOREIGN KEY (COURSE_ID) REFERENCES courses(ID) MATCH FULL ON DELETE CASCADE
               ); '''
 
         cursor.execute(create_table_query)
 
+        # le modifiche diventano definitive
         connection.commit()
     except (Exception, psycopg2.Error) as error:
         print("Error creating tables", error)
     finally:
+        # chiudo il cursore in finally
         cursor.close()
+
 
 def insert_names(connection):
     cursor = connection.cursor()
@@ -42,6 +50,7 @@ def insert_names(connection):
 
         nomi = [("Marco", "Torlaschi"), ("Roberta", "Latini"), ("Giorgo", "Franzini")]
 
+        # preparo il comando generico con %s e poi inserisco  valori in all'esecuzione
         insert = "INSERT INTO students (NAME,SURNAME) VALUES (%s,%s)"
 
         for n in nomi:
@@ -52,6 +61,8 @@ def insert_names(connection):
         print("Error inserting names", error)
     finally:
         cursor.close()
+
+
 def insert_courses(connection):
     cursor = connection.cursor()
 
@@ -70,22 +81,21 @@ def insert_courses(connection):
     finally:
         cursor.close()
 
-def insert_booking(connection):
+
+def insert_booking(connection, name, surname, course):
     cursor = connection.cursor()
 
     try:
+        cursor.execute("SELECT ID FROM  students  WHERE NAME=%s AND SURNAME=%s", (name, surname))
+        student_id = cursor.fetchone()[0]
+        cursor.execute("SELECT ID FROM  courses  WHERE NAME = %s ", (course,))
+        course_id = cursor.fetchone()[0]
 
-
-        corsi = [("SQL", "corso sql"), ("SPRING", "corso spring")]
-
-        insert = "INSERT INTO courses (NAME,DESCR) VALUES (%s,%s)"
-
-        for n in corsi:
-            cursor.execute(insert, n)
+        cursor.execute("INSERT INTO booking (STUDENT_ID,COURSE_ID) VALUES (%s,%s)", (str(student_id), str(course_id)))
 
         connection.commit()
     except (Exception, psycopg2.Error) as error:
-        print("Error inserting names", error)
+        print("Error inserting bookings", error)
     finally:
         cursor.close()
 
@@ -96,7 +106,6 @@ def delate_names(connetion):
     try:
         cursor.execute("DELETE FROM  students where name='Roberta' ")
 
-
         # connection.rollback()
         connection.commit()
     except (Exception, psycopg2.Error) as error:
@@ -104,13 +113,14 @@ def delate_names(connetion):
     finally:
         cursor.close()
 
+
 def drop_tables(connetion):
     cursor = connection.cursor()
 
     try:
+        cursor.execute("DROP TABLE  booking  ")
         cursor.execute("DROP TABLE  students  ")
         cursor.execute("DROP TABLE  courses  ")
-        cursor.execute("DROP TABLE  booking  ")
         connection.commit()
     except (Exception, psycopg2.Error) as error:
         print("Error deleting name", error)
@@ -121,25 +131,31 @@ def drop_tables(connetion):
 if __name__ == '__main__':
     print("esempi sql")
 
-
-
     try:
+
+        # apro la connessione
         connection = psycopg2.connect(user="postgres",
                                       password="password",
                                       host="127.0.0.1",
                                       port="5432",
                                       database="sample")
 
-
         #drop_tables(connection)
 
         create_table(connection)
 
-        cursor = connection.cursor()
-
         insert_names(connection)
 
         insert_courses(connection)
+
+
+        insert_booking(connection, "Marco", "Torlaschi", "SQL")
+        insert_booking(connection, "Marco", "Torlaschi", "SPRING")
+        insert_booking(connection, "Roberta", "Latini", "SPRING")
+
+        delate_names(connection)
+
+        cursor = connection.cursor()
 
         cursor.execute("SELECT * FROM  students  ")
         for r in cursor.fetchall():
@@ -153,14 +169,14 @@ if __name__ == '__main__':
         for r in cursor.fetchall():
             print(r, "\n")
 
-
         drop_tables(connection)
 
 
     except (Exception, psycopg2.Error) as error:
+        # catcho l'errore in connessione
         print("Error while connecting to PostgreSQL", error)
     finally:
-        # closing database connection.
+        # chiudo la connessione in finally per essere sicuro che avvenga sempre
         if (connection):
             connection.close()
             print("PostgreSQL connection is closed")
